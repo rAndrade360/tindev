b5cd904fa9190d1627143defee481134a17efec3
rAndrade360 <r.andrade@acad.ifma.edu.br>
2019-08-14 14:19:32 -0300

    Alterações de like e Deslike

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import Link from 'react-router-dom';
 import api from '../services/api'
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
@@ -20,34 +21,61 @@ export default function Main({ match}){
         loadUsers();
     }, [match.params.id]);//Toda vez que o id da url for alterdo, executa a função
 
+    async function handleLike(id){
+        await api.post(`devs/${id}/likes`, null, {
+            header:{
+                user: match.params.id,
+            } 
+        })
+
+        setUsers(users.filter(user => user._id !== id));
+    }
+
+    async function handleDeslike(id){
+        await api.post(`devs/${id}/deslikes`, null, {
+            header:{
+                user: match.params.id,
+            } 
+        })
+
+        setUsers(users.filter(user => user._id !== id));
+    }
+
     return (
         
 
         <div className="main-container">
-            <img src={logo} alt="Logo" />
-            <ul>
-                {users.map(user => (
-                    <li key={user._id}>
-                    <img src={user.avatar} alt={user.name}/>
-
-                    <footer>
-                        <strong>{user.name}</strong>
-                        <p>{user.bio}</p>
-                    </footer>
-
-                    <div className="figures">
-                        <button type="button">
-                            <img src={like} alt=""/>
-                        </button>
-                        <button type="button">
-                            <img src={dislike} alt=""/>
-                        </button>
+           <Link to='/'> <img src={logo} alt="Logo" /> </Link>
+            {users.length > 0? (
+                 <ul>
+                 {users.map(user => (
+                     <li key={user._id}>
+                     <img src={user.avatar} alt={user.name}/>
+ 
+                     <footer>
+                         <strong>{user.name}</strong>
+                         <p>{user.bio}</p>
+                     </footer>
+ 
+                     <div className="figures">
+                         <button type="button" onClick={() => handleLike(user._id)}>
+                             <img src={like} alt=""/>
+                         </button>
+                         <button type="button" onClick = {() => handleDeslike(user._id)}>
+                             <img src={dislike} alt=""/>
+                         </button>
+                     </div>
+                 </li>
+ 
+                 ))}                
+                
+             </ul>
+            ) : (
+                    <div className="empty">
+                        acabou :(
                     </div>
-                </li>
-
-                ))}                
-               
-            </ul>
+            )}
+           
         </div>
     )
 }
