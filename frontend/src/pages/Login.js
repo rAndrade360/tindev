3e37270f91d98a728b8fbfab02a1d8e4a5cc6e93
rAndrade360 <r.andrade@acad.ifma.edu.br>
2019-08-14 10:53:59 -0300

    Últimas alterações

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from '../assets/logo.svg'
+import api from '../services/api';
 import './Login.css';
 
-export default function Login(){
+
+export default function Login({history}){
+    const [username, setUsername] = useState('');
+
+   async function handeSubmit(e){
+        e.preventDefault();//Impede que a página redirecione
+
+       // console.log(username);
+        const response = await api.post('/devs', {
+            username,
+        });
+        const {_id} = response.data;
+        console.log(response);
+        history.push(`/dev/${_id}`); //Faz o redirecionamento
+    }
+
     return (
         <div className="login-container">
-             <form>
+             <form onSubmit={handeSubmit}>
                 <img src={logo} alt='Logo Tindev'/>
-                <input placeholder="Digite seu usuário no GitHub"/>
+                <input placeholder="Digite seu usuário no GitHub"
+                value={username}
+                 onChange = {e => setUsername(e.target.value)}/>
                 <button type="submit">Enviar</button>
              </form>
         </div>  
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,5 +1,53 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import api from '../services/api'
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+import logo from '../assets/logo.svg'
+import './Main.css'
 
-export default function Main(){
-    return <h1>Hello World</h1>
+export default function Main({ match}){
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        async function loadUsers(){
+                const response = await api.get('/devs', {
+                    headers: {
+                    user: match.params.id,
+                }
+                })
+                setUsers(response.data);
+        };
+
+        loadUsers();
+    }, [match.params.id]);//Toda vez que o id da url for alterdo, executa a função
+
+    return (
+        
+
+        <div className="main-container">
+            <img src={logo} alt="Logo" />
+            <ul>
+                {users.map(user => (
+                    <li key={user._id}>
+                    <img src={user.avatar} alt={user.name}/>
+
+                    <footer>
+                        <strong>{user.name}</strong>
+                        <p>{user.bio}</p>
+                    </footer>
+
+                    <div className="figures">
+                        <button type="button">
+                            <img src={like} alt=""/>
+                        </button>
+                        <button type="button">
+                            <img src={dislike} alt=""/>
+                        </button>
+                    </div>
+                </li>
+
+                ))}                
+               
+            </ul>
+        </div>
+    )
 }
